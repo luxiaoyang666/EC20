7b33f5af05a4af57feecaca89ec8476995ce5241
luxiaoyang666 <920916829@qq.com>
2020-07-13 06:42:36 +0100

    comport head

--- SMS/comport.h
+++ SMS/comport.h
@@ -3,35 +3,30 @@
  *                  All rights reserved.
  *
  *       Filename:  comport.h
- *    Description:  This head file of comport
+ *    Description:  This head file of comport.c
  *
- *        Version:  1.0.0(09/07/20)
+ *        Version:  2.0.0(11/07/20)
  *         Author:  LuXiaoyang <920916829@qq.com>
- *      ChangeLog:  1, Release initial version on "09/07/20 08:58:33"
+ *      ChangeLog:  1, Release initial version on "11/07/20 16:09:33"
  *                 
  ********************************************************************************/
 #ifndef  _COMPORT_H_
 #define  _COMPORT_H_
 
+#include <fcntl.h>
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <termios.h>
 #include <string.h>
-#include <sys/types.h>
+#include <termios.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <signal.h>
-#include <getopt.h>
-#include <strings.h>
 
-#define SERIALNAME_LEN 128
-#define SMSLENTH       128
+#define SERIALNAME_LEN 64
 
-typedef struct _st_SerialAttr {
+typedef struct _st_ComportAttr {
 
     int               fd;        //串口文件描述符
     int               BaudRate;  //波特率
@@ -41,31 +36,24 @@ typedef struct _st_SerialAttr {
     int               mSend_Len; //单次最大发送长度
     char              SerialName[SERIALNAME_LEN];  //串口名称
     struct termios    OldTermios;  //串口的原始属性
-}Serial_attr;
-
-/* 打开串口 */
-int comport_open(Serial_attr *attr);
-
-/* 关闭串口 */
-int comport_close(Serial_attr *attr);
-
-/* 初始化串口 */
-int comport_init(Serial_attr *attr);
-
-/* 向串口发送指令 */
-int comport_send(Serial_attr *attr,char *sbuf,int sbuf_len);
-
-/* 接收串口指令 */
-int comport_recv(Serial_attr *attr,char *rbuf,int rbuf_len,int timeout);
-
-/* 发送TEXT格式短信 */
-int text_sms(Serial_attr *attr,char *sms_buf,char *phone_num);
-
-#endif   /*   ----- #ifndef _COMPORT_H_  ----- */
+}ComportAttr;
 
 
+/* Use the serial port name(comport->SerialName) to open and assign the returned file descriptor to comport->fd */
+int comport_open(ComportAttr *comport);
+
+/* Set the serial port's baud rate, data bits, parity and other attributes, and set the related Peugeot bits for serial communication */
+int comport_init(ComportAttr *comport);
+
+/* Send the serial port command in the parameter and can handle the command with too long length */
+int comport_send(ComportAttr *comport,char *sbuf,int sbuf_len);
+
+/* Receive serial data, you can use select multiplexing to specify the time of read according to the value of timeout */
+int comport_recv(ComportAttr *comport,char *rbuf,int rbuf_len,int timeout);
+
+/* Close the serial port, clear the serial port buffer, and release the memory */
+int comport_close(ComportAttr *comport);
 
 
-
-
+#endif   /* ----- #ifndef _COMPORT_H_  ----- */
 
